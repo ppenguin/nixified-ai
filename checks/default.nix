4dd8ad89560884717af5e0e0ee9317c4e8050660
Ludvig Böklin <ludvig.boklin@proton.me>
2024-04-26 13:40:35 +0200

    small tidying up

--- checks/default.nix
+++ checks/default.nix
@@ -1,12 +1,11 @@
 { ... }:
 {
-  perSystem = { pkgs, system, comfyuiCfg, comfyuiModels, ... }: let
+  perSystem = { pkgs, comfyuiCfg, comfyuiModels, ... }: let
     models = comfyuiCfg.models;
-    allModels = comfyuiModels;
     installModels = pkgs.callPackage ../projects/comfyui/install-models.nix {};
   in {
     checks.modelInstallation = installModels {
-      configs = { inherit (allModels.configs) controlnet-v1_1_fe-sd15-tile; };
+      configs = { inherit (comfyuiModels.configs) controlnet-v1_1_fe-sd15-tile; };
     };
   };
 }
